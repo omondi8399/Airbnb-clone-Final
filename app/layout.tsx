d60b71babf64af712622d6e945a6fb76460e136d
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-19 14:06:43 +0300

    Updated App layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import Modal from "./components/modals/Modal"
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -18,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ClientOnly>
+          <Modal title="Hello world" isOpen />
         <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
