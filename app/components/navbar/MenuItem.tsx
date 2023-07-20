2dcb6cab6b6212406e20ae8e239ed92083449ff4
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-20 21:13:54 +0300

    Updated MenuItem

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -1,14 +1,28 @@
-'use client'
+'use client';
 
 interface MenuItemProps {
-    onClick: () => void;
-    label: string;
+  onClick: () => void;
+  label: string;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ onClick, label}) => {
-  return (
-    <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">{label}</div>
-  )
+const MenuItem: React.FC<MenuItemProps> = ({
+  onClick,
+  label
+}) => {
+  return ( 
+    <div 
+      onClick={onClick} 
+      className="
+        px-4 
+        py-3 
+        hover:bg-neutral-100 
+        transition
+        font-semibold
+      "
+    >
+      {label}
+    </div>
+   );
 }
-
-export default MenuItem
+ 
+export default MenuItem;
