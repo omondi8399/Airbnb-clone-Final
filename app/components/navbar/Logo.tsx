2c9f1b2db8dec9fccc2d8ee5b66f66092380d2e5
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-20 21:15:00 +0300

    Updated Logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -4,11 +4,18 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const Logo = () => {
-    const router = useRouter()
+  const router = useRouter();
 
-  return (
-    <Image alt="Logo" className="hidden md:block cursor-pointer" height="100" width="100" src="/images/logo.png" />
-  )
+  return ( 
+    <Image
+      onClick={() => router.push('/')}
+      className="hidden md:block cursor-pointer" 
+      src="/images/logo.png" 
+      height="100" 
+      width="100" 
+      alt="Logo" 
+    />
+   );
 }
-
-export default Logo
+ 
+export default Logo;
