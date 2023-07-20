578cc0da0f8348a302cb828094bb45a2c5a9fe06
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-20 21:12:48 +0300

    Updated Navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,24 +1,47 @@
-'use client'
+import { SafeUser } from "@/app/types";
 
-import Container from "../Container"
-import Logo from "./Logo"
-import Search from "./Search"
-import UserMenu from "./UserMenu"
+import Categories from "./Categories";
+import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
-const Navbar = () => {
-return (
-    <div className="fixed w-full bg-white z-10 shadow-sm">
-        <div className="py-4 border-b-[1px]">
-            <Container>
-                <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
-                    <Logo />
-                    <Search />
-                    <UserMenu />
-                </div>
-            </Container>
-        </div>
-    </div>
-)
+interface NavbarProps {
+  currentUser?: SafeUser | null;
 }
 
-export default Navbar
+const Navbar: React.FC<NavbarProps> = ({
+  currentUser,
+}) => {
+  return ( 
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+          py-4 
+          border-b-[1px]
+        "
+      >
+      <Container>
+        <div 
+          className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+        >
+          <Logo />
+          <Search />
+          <UserMenu currentUser={currentUser} />
+        </div>
+      </Container>
+    </div>
+    <Categories />
+  </div>
+  );
+}
+
+
+export default Navbar;
